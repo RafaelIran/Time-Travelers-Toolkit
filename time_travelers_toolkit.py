1526b7ae0bbb14c9c01fec3ecda4cd8cf25863ff
Rafael <rafael9.rr@gmail.com>
2026-05-04 16:58:02 +0100

    Time travelers toolkit completed

--- time_travelers_toolkit.py
+++ time_travelers_toolkit.py
@@ -5,4 +5,14 @@ import custom_module
 
 now = dt.now()
 
-print(now)
+possible_destinations = choice(['Brazil', 'Spain', 'UK', 'USA', 'Italy', 'Switzerland', 'Portugal'])
+
+target_year = randint(now.year, now.year + 50)
+
+base_cost = Decimal('280.30')
+
+cost_multiplier = abs(now.year - target_year)
+
+final_cost = Decimal(base_cost * cost_multiplier)
+
+print(custom_module.generate_time_travel_message(target_year, possible_destinations, final_cost))
