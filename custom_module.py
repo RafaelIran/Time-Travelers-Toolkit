f396a20af0176a3c917ea205af7de8e37316e258
Rafael <rafael9.rr@gmail.com>
2026-05-04 16:03:48 +0100

    Initial setup completed

---
+++ custom_module.py
@@ -0,0 +1,2 @@
+def generate_time_travel_message(year, destination, cost):
+    return f"The cost was {cost} from our trip during {year} to {destination}"
---
+++ time_travelers_toolkit.py
@@ -0,0 +1,8 @@
+from datetime import datetime as dt
+from decimal import Decimal
+from random import randint, choice
+import custom_module
+
+now = dt.now()
+
+print(now)
